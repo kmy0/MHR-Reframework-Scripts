d924d28c740058b80e044c56decfef0f8af9198e
kmy0 <93156259+kmy0@users.noreply.github.com>
2022-12-24 08:19:15 +0100

    Create singletons.lua

---
+++ reframework/autorun/QuestTimer/singletons.lua
@@ -0,0 +1,34 @@
+local singletons = {}
+
+singletons.guiman = nil
+singletons.questman = nil
+singletons.enemyman = nil
+
+function singletons.get_questman()
+    if not singletons.questman then
+        singletons.questman = sdk.get_managed_singleton('snow.QuestManager')
+    end
+    return singletons.questman
+end
+
+function singletons.get_guiman()
+    if not singletons.guiman then
+        singletons.guiman = sdk.get_managed_singleton('snow.gui.GuiManager')
+    end
+    return singletons.guiman
+end
+
+function singletons.get_enemyman()
+    if not singletons.enemyman then
+        singletons.enemyman = sdk.get_managed_singleton("snow.enemy.EnemyManager")
+    end
+    return singletons.enemyman
+end
+
+function singletons.init()
+	singletons.get_questman()
+	singletons.get_guiman()
+	singletons.get_enemyman()
+end
+
+return singletons
